d278de492519000d1c0440134eddd13f52ab76f7
Fortran <mykomurcu1@gmail.com>
2023-08-20 18:12:21 +0300

    a

--- index.js
+++ index.js
@@ -134,7 +134,7 @@ const wss = new webSocket.Server({ server });
 
 wss.on('connection', (ws, req) => {
   const cookies = req.headers.cookie;
-  console.log(req.headears.cookie);
+  console.log(req.headers.cookie);
   console.log(req.cookies);
   if (cookies) {
     const tokenCookieString = cookies.split(';').find(str => str.startsWith('token='));
