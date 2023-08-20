d2babafeeb943d51f829741ea12f575764c8fe05
Fortran <mykomurcu1@gmail.com>
2023-08-20 12:35:26 +0300

    asdasdasdfa

--- index.js
+++ index.js
@@ -17,6 +17,12 @@ app.use(cors({
   credentials: true,
   origin: 'http://localhost:5173'
 }));
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', "http://localhost:5173");
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 app.use(express.json());
 app.use(cookieParser());
 
